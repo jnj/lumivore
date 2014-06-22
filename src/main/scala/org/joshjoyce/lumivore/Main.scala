c9a48d2312b1196dee197722a9778ed97e81cab1
Josh Joyce <j_joyce2001@yahoo.com>
2014-06-22 02:34:07 -0500

    insert / query working

--- src/main/scala/org/joshjoyce/lumivore/Main.scala
+++ src/main/scala/org/joshjoyce/lumivore/Main.scala
@@ -6,21 +6,33 @@ import org.joshjoyce.lumivore.io.DirectoryPathStream
 import org.joshjoyce.lumivore.util.Implicits
 import org.jetlang.channels.MemoryChannel
 import org.jetlang.fibers.ThreadFiber
+import org.joshjoyce.lumivore.db.SqliteDatabase
 
 object Main {
   import Implicits._
 
   def main(args: Array[String]) {
-    val dir = args(0)
-    val paths = new DirectoryPathStream(new File(dir))
-    val outputChannel = new MemoryChannel[IndexRecord]
-    val fiber = new ThreadFiber()
-    fiber.start()
+//    val dir = args(0)
+//    val paths = new DirectoryPathStream(new File(dir))
+//    val outputChannel = new MemoryChannel[IndexRecord]
+    val database = new SqliteDatabase
+    database.connect()
+//    database.createTables()
+//
+//    val fiber = new ThreadFiber()
+//    fiber.start()
+//
+//    outputChannel.subscribe(fiber) {
+//      r => {
+//        database.insert(r)
+//        println(r)
+//      }
+//    }
+//
+//    val indexer = new Indexer(paths, outputChannel)
 
-    outputChannel.subscribe(fiber) {
-      println(_)
-    }
+    database.queryPhotos().foreach(println)
 
-    val indexer = new Indexer(paths, outputChannel)
+
   }
 }
--- src/main/scala/org/joshjoyce/lumivore/db/SqliteDatabase.scala
+++ src/main/scala/org/joshjoyce/lumivore/db/SqliteDatabase.scala
@@ -1,21 +1,75 @@
 package org.joshjoyce.lumivore.db
 
-import java.sql.{Statement, DriverManager, Connection}
+import java.sql._
+import org.joshjoyce.lumivore.index.IndexRecord
+import org.joshjoyce.lumivore.index.IndexRecord
+import java.nio.file.Paths
 
 class SqliteDatabase {
+
   Class.forName("org.sqlite.JDBC")
-  var conn: Connection = _
+  private var conn: Connection = _
+  private var connected = false
 
   def connect() {
     conn = DriverManager.getConnection("jdbc:sqlite:photos.db")
+    connected = true
   }
 
   def createTables() {
+    ensureConnected()
     val sql =
       """
+        |DROP TABLE IF EXISTS PHOTOS;
         |CREATE TABLE PHOTOS (ID INTEGER PRIMARY KEY, FILE_PATH TEXT UNIQUE, HASH TEXT);
       """.stripMargin
     executeUpdate(sql)
+  }
+
+  def queryPhotos() = {
+    ensureConnected()
+    val sql =
+      """
+        |SELECT ID, FILE_PATH, HASH FROM PHOTOS;
+      """.stripMargin
+    var stmt: PreparedStatement = null
+    var results: ResultSet = null
+    var list = List.empty[IndexRecord]
+    try {
+      stmt = conn.prepareStatement(sql)
+      results = stmt.executeQuery()
+      while (results.next()) {
+        val i = IndexRecord(Paths.get(results.getString("FILE_PATH")), results.getString("HASH"))
+        list = i :: list
+      }
+    } finally {
+      if (results != null) {
+        results.close()
+      }
+      if (stmt != null) {
+        stmt.close()
+      }
+    }
+    list
+  }
+
+  def insert(record: IndexRecord) {
+    ensureConnected()
+    val sql =
+      """
+        |INSERT INTO PHOTOS(FILE_PATH, HASH) VALUES (?, ?);
+      """.stripMargin
+    var preparedStatement: PreparedStatement = null
+    try {
+      preparedStatement = conn.prepareStatement(sql)
+      preparedStatement.setString(1, record.path.toString)
+      preparedStatement.setString(2, record.digest)
+      preparedStatement.execute()
+    } finally {
+      if (preparedStatement != null) {
+        preparedStatement.close()
+      }
+    }
   }
 
   def executeUpdate(sql: String) {
@@ -27,6 +81,12 @@ class SqliteDatabase {
       if (statement != null) {
         statement.close()
       }
+    }
+  }
+
+  private def ensureConnected() {
+    if (!connected) {
+      throw new IllegalStateException("not connected")
     }
   }
 }
