a640eb2ec717cf27bb43f0935817c6dbb98be8eb
Josh Joyce <j_joyce2001@yahoo.com>
2018-11-19 21:14:41 -0600

    wip converting to Java

---
+++ src/main/java/org/joshjoyce/lumivore/db/SetupDatabase.java
@@ -0,0 +1,12 @@
+package org.joshjoyce.lumivore.db;
+
+public class SetupDatabase {
+    public static void main(String[] args) {
+        var db = new SqliteDatabase();
+        db.connect();
+        db.createTables();
+        db.createIndexes();
+        //Set("jpg", "dng", "rw2", "orf").foreach(db.addExtension)
+        System.out.println("ok");
+    }
+}
