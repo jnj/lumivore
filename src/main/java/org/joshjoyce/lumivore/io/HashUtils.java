d9436db0a65e45ac770f9a9b0f565c6f6dcb04ec
Josh Joyce <j_joyce2001@yahoo.com>
2018-12-02 16:51:25 -0600

    use aws tree hash util

--- src/main/java/org/joshjoyce/lumivore/io/HashUtils.java
+++ src/main/java/org/joshjoyce/lumivore/io/HashUtils.java
@@ -1,42 +1,13 @@
 package org.joshjoyce.lumivore.io;
 
-import javax.xml.bind.DatatypeConverter;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import com.amazonaws.services.glacier.TreeHashGenerator;
+
 import java.nio.file.Path;
-import java.security.DigestInputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class HashUtils {
-    private static MessageDigest digest;
-
-    static {
-        try {
-            digest = MessageDigest.getInstance("SHA-1");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public static String hashContents(Path path) {
         try {
-            byte[] sha1Bytes;
-
-            try (var in = new FileInputStream(path.toFile());
-                 var bis = new BufferedInputStream(in);
-                 var dis = new DigestInputStream(bis, digest)) {
-                var buffer = new byte[8192];
-
-                while (dis.read(buffer) != -1) {
-                    // nothing to do
-                }
-
-                sha1Bytes = HashUtils.digest.digest();
-            }
-            var hash = DatatypeConverter.printHexBinary(sha1Bytes);
-            HashUtils.digest.reset();
-            return hash;
+            return TreeHashGenerator.calculateTreeHash(path.toFile());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
