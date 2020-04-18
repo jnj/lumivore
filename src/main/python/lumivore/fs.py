1c3cb991854b1a6d30a1c637f330139b8e2bae14
Josh Joyce <j_joyce2001@yahoo.com>
2020-04-18 11:26:49 -0500

    added case sensitivity flag

--- src/main/python/lumivore/fs.py
+++ src/main/python/lumivore/fs.py
@@ -14,7 +14,7 @@ def eachdir(root):
                 yield subdir
 
 
-def eachfile(root, extensions):
+def eachfile(root, extensions, case_sensitive=False):
     """
     Generator for every file that has a name ending
     with one of the given extensions. This will recursively
@@ -24,5 +24,14 @@ def eachfile(root, extensions):
         entries = os.listdir(directory)
         for entry in entries:
             fullentry = os.path.join(directory, entry)
-            if os.path.isfile(fullentry) and any(fullentry.endswith(ext) for ext in extensions):
+            if os.path.isfile(fullentry) and \
+                    any(extension_match(fullentry, ext, case_sensitive) for ext in extensions):
                 yield fullentry
+
+
+def extension_match(filepath, extension, case_sensitive):
+    if case_sensitive:
+        return filepath.endswith(extension)
+
+    lowercasepath = filepath.lower()
+    return lowercasepath.endswith(extension.lower())
